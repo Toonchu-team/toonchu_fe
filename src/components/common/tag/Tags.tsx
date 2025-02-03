9e179330503ccbcfad6f92f4339217e30ac64bf8
xuuwon <dumpling1011@gmail.com>
2025-02-03 13:25:39 +0900

    style: 태그 컴포넌트 호버 시 색상 설정

--- src/components/common/tag/Tags.tsx
+++ src/components/common/tag/Tags.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Tags = ({ tag }: { tag: string }) => {
   return (
-    <div className="inline-block border border-1-main-grey rounded-md sm:rounded-lg px-1.5 py-0.5 sm:px-2 sm:py-1">
+    <div className="inline-block border border-1-main-grey rounded-md sm:rounded-lg px-1.5 py-0.5 sm:px-2 sm:py-1 cursor-pointer hover:bg-bg-yellow-01">
       <p className="text-main-text text-[8px] sm:text-base">#{tag}</p>
     </div>
   );
