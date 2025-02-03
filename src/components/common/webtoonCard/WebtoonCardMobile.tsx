f088e8ffe7fee9708d5d20c1c0b9aa1d66b430d5
xuuwon <dumpling1011@gmail.com>
2025-02-03 16:47:51 +0900

    style: 뱃지 호버 시 색상 변경 추가 및 Tailwind CSS 정렬 수정

--- src/components/common/webtoonCard/WebtoonCardMobile.tsx
+++ src/components/common/webtoonCard/WebtoonCardMobile.tsx
@@ -22,21 +22,21 @@ const WebtoonCardMobile = () => {
   ];
 
   return (
-    <div className="flex w-[350px] h-[120px] drop-shadow-lg">
+    <div className="flex h-[120px] w-[350px] drop-shadow-lg">
       <Image
         src="/image.png"
         alt=""
         width={100}
         height={86}
-        className="rounded-tl-xl rounded-bl-xl"
+        className="rounded-bl-xl rounded-tl-xl"
       />
-      <div className="flex flex-col w-80 border border-1 bg-white rounded-tr-xl rounded-br-xl">
+      <div className="border-1 flex w-80 flex-col rounded-br-xl rounded-tr-xl border bg-white">
         <Image
           src="/naverSquare.png"
           alt="naverLogo"
           width={30}
           height={30}
-          className="self-end fixed rounded-tr-xl"
+          className="fixed self-end rounded-tr-xl"
         />
         <div className="flex flex-col gap-1 p-2">
           <div className="flex flex-wrap gap-1">
@@ -49,7 +49,7 @@ const WebtoonCardMobile = () => {
             <p className="text-xs">미래의 골동품 가게</p>
             <p className="text-[10px] text-main-text">구아진</p>
           </div>
-          <div className="flex flex-wrap gap-1 h-[50px] overflow-y-auto scrollbar-hide">
+          <div className="flex h-[50px] flex-wrap gap-1 overflow-y-auto scrollbar-hide">
             {tags.map((tag, index) => {
               return <Tags key={index} tag={tag} />;
             })}
