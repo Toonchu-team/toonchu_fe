34e4e9dc18dfb2c7cfe7101deeee93ca206325d8
kosmosticlay <kosmosticlay@gmail.com>
2025-02-06 10:59:09 +0900

    feat: Tailwind 설정 추가
    - 반응형 유틸리티 (desktop-only, tablet-only, mobile-only)
    - Swiper 유틸리티티
    - 커스텀 스크롤바

--- src/hooks/useResponsive.ts
+++ src/hooks/useResponsive.ts
@@ -2,28 +2,18 @@
 
 import { useMediaQuery } from "react-responsive";
 
-interface ResponsiveOptions {
-  isDesktop?: string;
-  isTablet?: string;
-  isMobile?: string;
-}
-
 interface ResponsiveValues {
   isDesktop: boolean;
   isTablet: boolean;
   isMobile: boolean;
 }
 
-const useResponsive = (options: ResponsiveOptions = {}): ResponsiveValues => {
-  const {
-    isDesktop = "(min-width: 1025px)",
-    isTablet = "(min-width: 769px) and (max-width: 1024px)",
-    isMobile = "(max-width: 768px)",
-  } = options;
-
-  const desktop = useMediaQuery({ query: isDesktop });
-  const tablet = useMediaQuery({ query: isTablet });
-  const mobile = useMediaQuery({ query: isMobile });
+const useResponsive = (): ResponsiveValues => {
+  const desktop = useMediaQuery({ query: "(min-width: 1025px)" });
+  const tablet = useMediaQuery({
+    query: "(min-width: 769px) and (max-width: 1024px)",
+  });
+  const mobile = useMediaQuery({ query: "(max-width: 768px)" });
 
   return {
     isDesktop: desktop,
