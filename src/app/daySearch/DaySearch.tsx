3764fe93724e1ed927bd0039874d77aaf751bd56
xuuwon <dumpling1011@gmail.com>
2025-02-10 19:20:28 +0900

    refactor: 파일 이름 수정

--- src/app/daySearch/DaySearch.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DaySearch = () => {
-  return <div>연재별 검색 페이지</div>;
-};
-
-export default DaySearch;
--- src/app/tag-search/TagSearch.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TagSearch = () => {
-  return <div>태그별 검색 페이지</div>;
-};
-
-export default TagSearch;
