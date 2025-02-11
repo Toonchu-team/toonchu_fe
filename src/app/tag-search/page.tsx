cc41513e4d7ad22e24043a5bb47db7f63dfaca48
xuuwon <dumpling1011@gmail.com>
2025-02-11 16:00:18 +0900

    feat: 태그별 검색 페이지 생성 및 반응형 구현

--- src/app/tag-search/page.tsx
+++ src/app/tag-search/page.tsx
@@ -1,7 +1,16 @@
-export default function TagSearch() {
-  return (
-    <div>
-      <h1>태그별 검색 컴포넌트</h1>
-    </div>
-  );
+"use client";
+
+import TagSearchMobile from "./TagSearchMobile";
+import TagSearchDesktop from "./TagSearchDesktop";
+import { BreakpointType } from "@/stores/breakptStore";
+import useBreakpoint from "@/hooks/useBreakpoint";
+
+export default function TagSearch({
+  initialBreakpoint,
+}: {
+  initialBreakpoint: BreakpointType;
+}) {
+  const breakpoint = useBreakpoint(initialBreakpoint);
+
+  return breakpoint == "mobile" ? <TagSearchMobile /> : <TagSearchDesktop />;
 }
