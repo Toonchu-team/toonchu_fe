afa03880892f3341c842378786bf3f1aa2984571
xuuwon <dumpling1011@gmail.com>
2025-02-14 13:20:49 +0900

    rename: 파일명 수정

--- src/app/tag-search/page.tsx
+++ src/app/tag-search/page.tsx
@@ -1,7 +1,7 @@
 import { breakpointDetect } from "@/lib/utils/breakpointDetect";
 import TagSearchClient from "./TagSearchClient";
 
-export default async function Main() {
+export default async function tagSearch() {
   const initialBreakpoint = await breakpointDetect();
 
   return <TagSearchClient initialBreakpoint={initialBreakpoint} />;
